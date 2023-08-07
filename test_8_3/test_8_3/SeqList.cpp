c8f4eb79974c3d7ca1e3ace3ff8f2d60d23f482f
TT982020 <xtli982020@outlook.com>
2023-08-07 18:27:01 +0800

    顺序表实现

--- test_8_3/test_8_3/SeqList.cpp
+++ test_8_3/test_8_3/SeqList.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "SeqList.h"
 void SLInit(SL* ps) {
+	assert(ps != NULL);
 	ps->list = (SLDataType*)malloc(sizeof(SLDataType) * 4);
 	if (ps->list == NULL)
 	{
@@ -13,12 +14,14 @@ void SLInit(SL* ps) {
 
 
 void SLDestroy(SL* ps) {
+	assert(ps != NULL);
 	free(ps->list);
 	ps->size = 0;
 	ps->capacity = 0;
 	ps->list = NULL;
 }
 void CheckCapacity(SL* ps) {
+	assert(ps != NULL);
 	if (ps->size == ps->capacity)
 	{
 		SLDataType* tmp = (SLDataType*)realloc(ps->list, sizeof(SLDataType) * 2 * ps->capacity);
@@ -33,11 +36,13 @@ void CheckCapacity(SL* ps) {
 }
 
 void SLPushBack(SL* ps, SLDataType data) {
+	assert(ps != NULL);
 	CheckCapacity(ps);
 	ps->list[ps->size++] = data;
 }
 
 void SLPrint(SL* ps) {
+	assert(ps != NULL);
 	for (int i = 0; i < ps->size; i++)
 	{
 		printf("%d ", ps->list[i]);
@@ -46,11 +51,13 @@ void SLPrint(SL* ps) {
 }
 
 void SLPopBack(SL* ps){
+	assert(ps != NULL);
 	assert(ps->size > 0);
 	ps->size--;
 }
 
 void SLPushFront(SL* ps, SLDataType data) {
+	assert(ps != NULL);
 	CheckCapacity(ps);
 	int end = ps->size - 1;
 	while (end >= 0)
@@ -63,6 +70,7 @@ void SLPushFront(SL* ps, SLDataType data) {
 }
 
 void SLPopFront(SL* ps) {
+	assert(ps != NULL);
 	assert(ps->size > 0);
 	int start = 0;
 	while (start <= ps->size - 2)
@@ -75,6 +83,7 @@ void SLPopFront(SL* ps) {
 
 //在pos位置前插入data
 void SLInsert(SL* ps, int pos, SLDataType data) {
+	assert(ps != NULL);
 	assert(pos >= 0 && pos <= ps->size);
 	CheckCapacity(ps);
 	int end = ps->size - 1;
@@ -89,6 +98,7 @@ void SLInsert(SL* ps, int pos, SLDataType data) {
 
 //删除pos位置的值
 void SLErase(SL* ps, int pos) {
+	assert(ps != NULL);
 	assert(pos >= 0 && pos < ps->size);
 	int begin = pos;
 	while (pos < ps->size - 1)
@@ -100,6 +110,7 @@ void SLErase(SL* ps, int pos) {
 }
 
 int SLFind(SL* ps, SLDataType data) {
+	assert(ps != NULL);
 	for (int i = 0; i < ps->size; i++)
 	{
 		if (ps->list[i] == data)
@@ -108,4 +119,10 @@ int SLFind(SL* ps, SLDataType data) {
 		}
 	}
 	return -1;
+}
+
+void SLModify(SL* ps, int pos, SLDataType data) {
+	assert(ps);
+	assert(pos >= 0 && pos < ps->size);
+	ps->list[pos] = data;
 }
