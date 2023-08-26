c3743809aa43ab54d17d96ff748a5d0463ff9ee7
TT982020 <xtli982020@outlook.com>
2023-08-26 16:32:11 +0800

    二叉树遍历初步

--- Heap/Heap/test.c
+++ Heap/Heap/test.c
@@ -95,8 +95,62 @@ void PrintTopK(int k) {
 	}
 }
 
+typedef int BTDataType;
+typedef struct BinaryTreeNode {
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+
+BTNode* BuyNode(BTDataType x) {
+	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+	if (node == NULL)
+	{
+		perror("malloc failed");
+		return NULL;
+	}
+	node->data = x;
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+//简单建树的过程
+BTNode* CreatBinaryTree()
+{
+	BTNode* node1 = BuyNode(1);
+	BTNode* node2 = BuyNode(2);
+	BTNode* node3 = BuyNode(3);
+	BTNode* node4 = BuyNode(4);
+	BTNode* node5 = BuyNode(5);
+	BTNode* node6 = BuyNode(6);
+
+	node1->left = node2;
+	node1->right = node4;
+	node2->left = node3;
+	node4->left = node5;
+	node4->right = node6;
+	return node1;
+}
+
+void PreOrder(BTNode* root) {
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+	printf("%d ", root->data);
+	PreOrder(root->left);
+	PreOrder(root->right);
+}
+
+
+//int main() {
+//	//createData();
+//	int k = 10;
+//	PrintTopK(k);
+//}
+
 int main() {
-	//createData();
-	int k = 10;
-	PrintTopK(k);
+	BTNode* root =  CreatBinaryTree();
+	PreOrder(root);
 }
