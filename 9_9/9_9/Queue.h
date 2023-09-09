2eb99d32f5a9b33dec6d4ad225d55689aac4df8b
TT982020 <xtli982020@outlook.com>
2023-09-09 18:42:39 +0800

    删除了一些冗余文件

--- 9_9/9_9/Queue.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace a {
-
-	void Queuenit() {
-		cout << "Queuenit()" << endl;
-	}
-}
--- 9_9/9_9/Stack.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace a {
-	void StackInit() {
-		cout << "StackInit()" << endl;
-	}
-}
